cac9944a78208cf09d19fc8e5bf242b4554cab9d
captHertz <chris.mcphan@gmail.com>
2024-09-29 23:08:01 +1000

    added awesomewm

--- widgets/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  icon     = require("widgets.icon_wgts"),
-  layout   = require("widgets.layout_wgts"),
-  --prompt   = require("widgets.prompt_wgts"),
-  taglist  = require("widgets.taglist_wgts"),
-  tasklist = require("widgets.tasklist_wgts"),
-  time     = require("widgets.time_wgts"),
-  titlebar = require("widgets.titlebar_wgts"),
-}
